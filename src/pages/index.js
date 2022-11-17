9fcb770314b2370d877da8a1cc8851d722613493
Aaron Eveleth <aaron.eveleth@phoenix.edu>
2022-11-17 14:27:11 -0700

    update with purchase logic

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,34 @@
+'use client';
 import React, { useState } from "react";
 import { Web3Button } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
-// import Whitelist from "../components/Whitelist";
+import Image from 'next/image'
+
+
+import {
+  CardContent,
+  CardHeader,
+  CardActions,
+  Typography,
+  Stack,
+} from "@mui/material";
+
+import Web3 from 'web3';
 
 export default function Home() {
   const [amountToClaim, setAmountToClaim] = useState("");
   const [balance, setBalance] = useState(0);
+  const [approval, setApproval] = useState(false);
+  const [purchased, setPurchased] = useState(false);
+
+  const [currentAddress, setCurrentAddress] = useState("");
+
+  const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+  web3.eth.getAccounts().then((addresses) => setCurrentAddress(addresses[0]));
 
   return (
     <div className={styles.container}>
+      <h1>Your address: {currentAddress}</h1>
       {/* <Whitelist/> */}
       <h2 className={styles.title}>Claim Tokens</h2>
       <p className={styles.explain}>
@@ -36,7 +56,7 @@ export default function Home() {
         <Web3Button
           accentColor="#5204BF"
           colorMode="dark"
-          contractAddress="0x68Beb805242D2113341a8338AaF8F55776A825f8"
+          contractAddress="0x070CD3393A57968166d6B82699D9b4e4E9fF0bde"
           action={(contract) => contract.erc20.claim(amountToClaim)}
           onSuccess={() => alert("Claimed!")}
           onError={(err) => alert(err)}
@@ -48,12 +68,12 @@ export default function Home() {
       <Web3Button
           accentColor="#5204BF"
           colorMode="dark"
-          contractAddress="0x68Beb805242D2113341a8338AaF8F55776A825f8"
+          contractAddress="0x070CD3393A57968166d6B82699D9b4e4E9fF0bde"
           action={(contract) => contract.balance()}
           onSuccess={(res) => setBalance(res.displayValue)}
           onError={(err) => alert(err)}
         >
-          {balance ? balance : "Click to check balance"}
+          {balance ? ("You have " + balance) : "Click to check balance"}
         </Web3Button>
       </div>
       <div>
@@ -69,6 +89,51 @@ export default function Home() {
         <div>
           <h2>Woah!</h2>
           <p>You got 5. You gained access to the super secret stuff!</p>
+          <div style={{background: "lightgray"}}>
+          <Stack direction="row" sx={{mt: "10px !important"}}>
+          <Image src="/thirdweb.svg" alt="Vercel Logo" width={72} height={16} sx={{mt: "10px !important"}} />
+                <Typography sx={{ml: 10}}>
+                    Test Course
+                </Typography>
+            </Stack>
+            <CardHeader />
+            <CardContent>
+              <Typography>
+                Test Course info
+              </Typography>
+              <Image src="/studying.webp" alt="Vercel Logo" width={100} height={80}/>
+            </CardContent>
+                <CardActions >
+                    <Stack spacing={2} sx={{ width: "100%" }}>
+                      {approval 
+                      ? 
+                        <Web3Button
+                          isDisabled={purchased}
+                          accentColor="#5204BF"
+                          colorMode="dark"
+                          contractAddress="0x070CD3393A57968166d6B82699D9b4e4E9fF0bde"
+                          action={(contract) => contract.burnFrom(currentAddress, 5.0)}
+                          onSuccess={(res) => {setPurchased(true); setBalance(balance => (balance - 5))}}
+                          onError={(err) => alert(err)}
+                        >
+                          {purchased ? "purchased": "Buy Course - 5 RISE"}
+                        </Web3Button>
+                      :
+                        <Web3Button
+                          accentColor="#5204BF"
+                          colorMode="dark"
+                          contractAddress="0x070CD3393A57968166d6B82699D9b4e4E9fF0bde"
+                          action={(contract) => contract.setAllowance(currentAddress, 5)}
+                          onSuccess={(res) => setApproval(true)}
+                          onError={(err) => alert(err)}
+                        >
+                          Approve Contract to spend 5 rise
+                        </Web3Button>
+                      }
+                    
+                    </Stack>
+                </CardActions>
+            </div>
         </div>
         }
       </div>
